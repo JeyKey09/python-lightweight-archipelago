f0051506bf6fe4b8ade3a7266da49d507763ec9b
JeyKey09 <mathias-jk@live.no>
2025-02-01 18:25:04 +0100

    moved sending to another function and allowed a periodic one to be sent

--- client/__init__.py
+++ client/__init__.py
@@ -8,7 +8,7 @@ from .packets.client import Connect, GetDataPackage, Sync
 from queue import Queue
 from .packets.core.enums import ItemsHandlingFlags
 from .packets import  encode_packet, decode_packet
-
+from threading import Lock
 #Import types into the namespace to be able to decode them
 from .packets.server import Connected, ConnectionRefused, DataPackage, LocationInfo, PrintJSON, ReceivedItems, RoomInfo
 from .packets.core import DataPackageObject, Version
@@ -44,29 +44,39 @@ class Client():
         self.client_config = client_config
         self.game_config = game_config
         self.active = True
+        self._handshake_done = False
         self.room_info = None
         self._packages_to_be_sent = Queue()
         self._item_index = 0
         self._connection = None
+        self._sender_lock = Lock()
         
     def add_package(self, package):
         self._packages_to_be_sent.put(package)
     
-    async def __send_packages(self):
-        while(self.active and self._connection.state is State.OPEN):
-            if not self._packages_to_be_sent.empty():
-                packages = [self._packages_to_be_sent.get() for _ in range(min(self._packages_to_be_sent.qsize(), 10))]
-                try:
-                    for i,package in enumerate(packages):
-                        packages[i] = encode_packet(package)
-                        packages[i]["cmd"] = type(package).__name__
-                    await self._connection.send(json.dumps(packages))
-                except ConnectionClosed:
-                    return
-                except Exception as e:
-                    print(f"Something went wrong, under the process {e.args}")
-            await asyncio.sleep(0.1)
+    async def __send_packages(self, packets : List):
+        if not isinstance(packets, list):
+            packets = [packets]
+        self._sender_lock.acquire()
+        try:
+            for i,package in enumerate(packets):
+                packets[i] = encode_packet(package)
+                packets[i]["cmd"] = type(package).__name__
+
+            await self._connection.send(json.dumps(packets))
+        except ConnectionClosed:
+                return
+        except Exception as e:
+                logging.info(f"Something went wrong, under the process {e.args}")
+        self._sender_lock.release()
     
+    async def __packet_sender(self):
+        while(self.active and self._connection.state is State.OPEN):
+            if self._handshake_done and not self._packages_to_be_sent.empty():
+                packages = [self._packages_to_be_sent.get() for _ in range(min(self._packages_to_be_sent.qsize(), 10))]
+                await self.__send_packages(packets=packages)
+            await asyncio.sleep(0.1)
+
     async def __process_server_packages(self):
         while(self.active and self._connection.state is State.OPEN):
             try:
@@ -85,6 +95,7 @@ class Client():
                         case Connected():
                             logging.info("Connection established to the archipelago server")
                             self.connected = packet
+                            self._handshake_done = True
 
                         case ConnectionRefused():
                             logging.exception("The connection was refused, may be due to wrong client info")
@@ -141,9 +152,9 @@ class Client():
             if not cache_exist(checksum):
                 missing_games.append(game)
         if len(missing_games) > 0:
-            self._packages_to_be_sent.put(GetDataPackage(missing_games))
+            send = self.__send_packages(GetDataPackage(missing_games))
         else:
-            self._packages_to_be_sent.put(Connect(self.client_config.password, self.game_config.game, 
+            self.__send_packages(Connect(self.client_config.password, self.game_config.game, 
                                                   self.client_config.player, self.client_config.client, 
                                                   self.client_config.version, self.game_config.items_handling, [], True))
     def _handle_datapackage(self, packet):
@@ -161,7 +172,7 @@ class Client():
                 counter = 0
                 await asyncio.gather(
                     asyncio.create_task(self.__process_server_packages()),
-                    asyncio.create_task(self.__send_packages())
+                    asyncio.create_task(self.__packet_sender())
                 )
                 if self.active:
                     logging.error("The connection to the archipelago server was closed unexpected, will try to reconnect")
